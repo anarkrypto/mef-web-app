d203871628c91b2d5486f2cbd40ba72b0e0d4c1d
Illya Gerasymchuk <illya@iluxonchik.me>
2024-11-01 10:56:21 +0000

    chore: framework setup

---
+++ next.config.js
@@ -0,0 +1,18 @@
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'standalone',
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '2mb',
+    },
+  },
+  serverExternalPackages: ['@prisma/client'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...(config.externals || []), '@prisma/client'];
+    }
+    return config;
+  },
+};
+
+export default config; 
