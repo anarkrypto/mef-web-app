07411414e20e4d1952c34378f4cc2ba35e78fe82
anarkrypto <anarkrypto@gmail.com>
2025-02-14 18:13:29 -0300

    refactor: enhance QueryClient configuration to handle errors with toast notifications

--- src/contexts/QueryClientProvider.tsx
+++ src/contexts/QueryClientProvider.tsx
@@ -2,11 +2,22 @@
 
 import { ReactNode } from 'react'
 import {
+	QueryCache,
 	QueryClient,
 	QueryClientProvider as TanStackQueryClientProvider,
 } from '@tanstack/react-query'
+import { toast } from '@/hooks'
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient({
+	queryCache: new QueryCache({
+		onError: error =>
+			toast({
+				title: 'Error',
+				description: error.message,
+				variant: 'destructive',
+			}),
+	}),
+})
 
 export function QueryClientProvider({ children }: { children: ReactNode }) {
 	return (
