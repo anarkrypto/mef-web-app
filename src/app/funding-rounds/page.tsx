6beac5a67886819787be0657309c574f856ed5fd
anarkrypto <anarkrypto@gmail.com>
2025-02-15 14:37:46 -0300

    feat: enhance funding rounds page responsiveness with sorting options and improved layout

--- src/app/funding-rounds/page.tsx
+++ src/app/funding-rounds/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Search } from 'lucide-react'
+import { ArrowDownNarrowWide, ArrowDownWideNarrow, Search } from 'lucide-react'
 import { FundingRoundCard } from '@/components/funding-rounds/FundingRoundCard'
 import { Input } from '@/components/ui/input'
 import {
@@ -18,6 +18,7 @@ import { useFundingRounds } from '@/hooks/use-funding-rounds'
 import { FundingRoundsSkeleton } from './loading'
 import { fundingRoundSortSchema, SortOption } from '@/services'
 import { useQueryState } from 'nuqs'
+import { Button } from '@/components/ui/button'
 
 const sortByOptions: {
 	value: SortOption['sortBy']
@@ -25,7 +26,7 @@ const sortByOptions: {
 }[] = [
 	{ value: 'startDate', label: 'Date' },
 	{ value: 'status', label: 'Status' },
-	{ value: 'totalBudget', label: 'Total Funding' },
+	{ value: 'totalBudget', label: 'Budget' },
 ]
 
 export default function FundingRounds() {
@@ -57,33 +58,67 @@ export default function FundingRounds() {
 
 	return (
 		<div>
-			<div className="mb-8 flex items-center justify-between">
-				<h1 className="text-2xl font-bold text-[#2D2D2D]">Funding Rounds</h1>
-				<div className="flex gap-4">
-					<div className="relative">
+			<div className="mb-8">
+				<div className="mb-4 text-secondary">
+					<h1 className="text-2xl font-bold">Funding Rounds</h1>
+					<p>
+						Explore and participate in funding rounds for community proposals.
+					</p>
+				</div>
+				<div className="flex justify-between gap-4">
+					{/* <div className="relative block md:hidden">
+						<Button variant="outline" size="icon">
+							<Search />
+						</Button>
+					</div> */}
+					<div className="relative md:block">
 						<Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform text-gray-400" />
 						<Input
 							placeholder="Search rounds..."
 							value={searchQuery}
 							onChange={e => setSearchQuery(e.target.value)}
-							className="hidden w-[300px] pl-9 md:block"
+							className="w-[170px] pl-9 md:w-[300px]"
 						/>
 					</div>
-					<Select
-						value={sortBy}
-						onValueChange={value => setSortBy(value as SortOption['sortBy'])}
-					>
-						<SelectTrigger className="w-[180px]">
-							<SelectValue placeholder="Sort by" />
-						</SelectTrigger>
-						<SelectContent>
-							<SelectGroup>
-								{sortByOptions.map(option => (
-									<SelectItem value={option.value}>{option.label}</SelectItem>
-								))}
-							</SelectGroup>
-						</SelectContent>
-					</Select>
+					<div className="flex gap-2">
+						<Select
+							value={sortBy}
+							onValueChange={value => setSortBy(value as SortOption['sortBy'])}
+						>
+							<SelectTrigger className="w-[90px]">
+								<SelectValue placeholder="Sort by" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									{sortByOptions.map(option => (
+										<SelectItem value={option.value}>{option.label}</SelectItem>
+									))}
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+						<Select
+							value={sortOrder}
+							onValueChange={value =>
+								setSortOrder(value as SortOption['sortOrder'])
+							}
+						>
+							<SelectTrigger className="w-[50px]">
+								<SelectValue placeholder="Order by" />
+							</SelectTrigger>
+							<SelectContent>
+								<SelectGroup>
+									<SelectItem value={'asc'}>
+										<ArrowDownNarrowWide className="mr-1 inline h-5 w-5" />
+										Ascending
+									</SelectItem>
+									<SelectItem value={'desc'}>
+										<ArrowDownWideNarrow className="mr-1 inline h-5 w-5" />
+										Descending
+									</SelectItem>
+								</SelectGroup>
+							</SelectContent>
+						</Select>
+					</div>
 				</div>
 			</div>
 
