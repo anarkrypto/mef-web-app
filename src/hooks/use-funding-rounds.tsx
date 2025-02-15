c4d0797ae2e6b3410f8f4f7d485db36a3825a9a5
anarkrypto <anarkrypto@gmail.com>
2025-02-15 13:33:05 -0300

    feat: enhance useFundingRounds hook to support sorting options

--- src/hooks/use-funding-rounds.tsx
+++ src/hooks/use-funding-rounds.tsx
@@ -1,12 +1,23 @@
 import logger from '@/logging'
+import { SortOption } from '@/services'
 import { FundingRoundWithPhases } from '@/types/funding-round'
 import { useQuery } from '@tanstack/react-query'
 
-export function useFundingRounds() {
+export function useFundingRounds({
+	sortOption,
+}: { sortOption?: SortOption } = {}) {
+	const searchParams = new URLSearchParams()
+	if (sortOption) {
+		searchParams.set('sortBy', sortOption.sortBy)
+		searchParams.set('sortOrder', sortOption.sortOrder)
+	}
+
+	const url = `/api/funding-rounds?${searchParams.toString()}`
+
 	return useQuery<FundingRoundWithPhases[]>({
-		queryKey: ['funding-rounds'],
+		queryKey: [url],
 		queryFn: async () => {
-			const response = await fetch('/api/funding-rounds')
+			const response = await fetch(url)
 			if (!response.ok) {
 				logger.error('Failed to fetch funding rounds', response)
 				throw new Error('Failed to fetch funding rounds')
