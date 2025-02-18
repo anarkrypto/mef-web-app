2729dff43ef2ba50ce7f2c97b175f06c02abbb5c
anarkrypto <anarkrypto@gmail.com>
2025-02-18 19:16:54 -0300

    feat: enable authInterrupts in serverActions

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ const config = {
 		serverActions: {
 			bodySizeLimit: '2mb',
 		},
+		authInterrupts: true,
 	},
 	serverExternalPackages: ['@prisma/client'],
 	webpack: (config, { isServer }) => {
